e65dabe14d7b071a38a7437d99a975381b1d5743
JoanMarcBardes <71397864+JoanMarcBardes@users.noreply.github.com>
2020-10-21 20:25:38 +0200

    commit jm terminal

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -7,7 +7,7 @@ std::cout<< "Hello World"<<std::endl;
 int nil = 96;
 float isaac = 3.0f;
 int Jimmynator = 2 - 4 * (3 - 2 * 2.5f) + 3L / 3.1416;
-int JoanMarc = 7;
+int JoanMarc = 9 + 1 / 10;
 int David = 4;
 
 
