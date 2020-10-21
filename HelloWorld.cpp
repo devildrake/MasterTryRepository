68501af0efaa70d5527671d355a578708eda7377
isaac fernandez fontao <isaacfdez94@gmail.com>
2020-10-21 20:36:28 +0200

    commit 123

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -5,7 +5,7 @@ void main(){
 std::cout<< "Hello World"<<std::endl;
 
 int nil = 96;
-float isaac = 3.0f;
+float isaac = 10.0f;
 int Jimmynator = 2 - 4 * (3 - 2 * 2.5f) + 3L / 3.1416;
 int JoanMarc = 7;
 int iAmDavid = 400;
