a7701b4a69508d9edbb2a100f4dadcd4f101b8ab
Jaime Bea <jaimebeapz@gmail.com>
2020-10-21 19:51:04 +0200

    Change Jimmynator line

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,7 @@ std::cout<< "Hello World"<<std::endl;
 
 int joan = 0;
 float isaac = 1.0f;
-int Jimmynator =2;
+int Jimmynator = 2 - 4 * (3 - 2 * 2.5f) + 3L / 3.1416;
 int JoanMarc = 3; 
 int David = 4;
 
