3bda1dc3480954912fdf50db88a1a40bb2778952
Jaime Bea <jaimebeapz@gmail.com>
2020-10-21 20:38:16 +0200

    Jimmynator is 3

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -6,7 +6,7 @@ std::cout<< "Hello World"<<std::endl;
 
 int nil = 96;
 float isaac = 10.0f;
-int Jimmynator = 2 - 4 * (3 - 2 * 2.5f) + 3L / 3.1416;
+int Jimmynator = 3;
 int JoanMarc = 9 + 1 / 10;
 int iAmDavid = 400;
 std::cout << "SO I'm supposed to know shit " << std::endl;
